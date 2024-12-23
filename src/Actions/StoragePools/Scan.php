c233bbd1921bf0473312750fc6e87ca389dc58cb
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-12-24 02:20:09 +0300

    Scan storage pool is updated

--- src/Actions/StoragePools/Scan.php
+++ src/Actions/StoragePools/Scan.php
@@ -1,9 +1,13 @@
 <?php
 namespace NextDeveloper\IAAS\Actions\StoragePools;
 
+use Illuminate\Support\Facades\Log;
 use NextDeveloper\Commons\Actions\AbstractAction;
 use NextDeveloper\IAAS\Database\Models\Datacenters;
+use NextDeveloper\IAAS\Database\Models\StoragePools;
+use NextDeveloper\IAAS\Database\Models\StorageVolumes;
 use NextDeveloper\IAM\Database\Models\Users;
+use NextDeveloper\IAM\Database\Scopes\AuthorizationScope;
 
 class Scan extends AbstractAction
 {
@@ -13,19 +17,26 @@ class Scan extends AbstractAction
         'scan-failed:NextDeveloper\IAAS\StoragePools'
     ];
 
-    public function __construct(Datacenters $datacenters)
+    public function __construct(StoragePools $pool, $params = null, $previous = null)
     {
-        trigger_error('This action is not yet implemented', E_USER_ERROR);
-
-        $this->model = $datacenters;
+        $this->model = $pool;
 
         $this->queue = 'iaas';
 
-        parent::__construct();
+        parent::__construct($params, $previous);
     }
 
     public function handle()
     {
+        Log::info(__METHOD__ . ' | Scanning all storage volumes on pool: ' . $this->model->name);
 
+        $volumes = StorageVolumes::withoutGlobalScope(AuthorizationScope::class)
+            ->where('iaas_storage_pool_id', $this->model->id)
+            ->get();
+
+        foreach ($volumes as $volume) {
+            Log::info(__METHOD__ . ' | Scanning storage volume: ' . $volume->name . '/' . $volume->uuid);
+            dispatch(new \NextDeveloper\IAAS\Actions\StorageVolumes\Scan($volume));
+        }
     }
 }
