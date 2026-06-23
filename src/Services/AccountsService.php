f9b660a00f35b2fe1866e8805a8aef157f9d5f4f
IBRAHIM KHALIL ATTEIB YACOUB <i.khalilatteib@gmail.com>
2026-06-23 17:09:09 +0300

    feat: add methods to enable service for IAM accounts and adjust eligibility checks

--- src/Services/AccountsService.php
+++ src/Services/AccountsService.php
@@ -27,6 +27,52 @@ class AccountsService extends AbstractAccountsService
         return $account->fresh();
     }
 
+    public static function enable(Accounts $account) {
+        $account->update([
+            'is_service_enabled' => true
+        ]);
+
+        return $account->fresh();
+    }
+
+    public static function enableWithIamAccount(\NextDeveloper\IAM\Database\Models\Accounts|int|string $account)
+    {
+        $iamAccount = null;
+
+        if($account instanceof \NextDeveloper\IAM\Database\Models\Accounts) {
+            $iamAccount = $account;
+        }
+
+        if(is_int($account)) {
+            $iamAccount = \NextDeveloper\IAM\Database\Models\Accounts::withoutGlobalScope(AuthorizationScope::class)
+                ->where('id', $account)
+                ->first();
+        }
+
+        if(is_string($account) && Str::isUuid($account)) {
+            $iamAccount = \NextDeveloper\IAM\Database\Models\Accounts::withoutGlobalScope(AuthorizationScope::class)
+                ->where('uuid', $account)
+                ->first();
+        }
+
+        if(!$iamAccount) {
+            return null;
+        }
+
+        $iaasAccount = Accounts::withoutGlobalScope(AuthorizationScope::class)
+            ->where('iam_account_id', $iamAccount->id)
+            ->first();
+
+        if(!$iaasAccount) {
+            return self::create([
+                'iam_account_id' => $iamAccount->uuid,
+                'is_service_enabled' => true,
+            ]);
+        }
+
+        return self::enable($iaasAccount);
+    }
+
     public static function suspendWithIamAccount(\NextDeveloper\IAM\Database\Models\Accounts|int|string $account)
     {
         if(is_int($account)) {
